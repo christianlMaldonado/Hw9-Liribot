2e3d204327c0ad054f58b240b404fc7c4e21f4c8
Christian Maldonado <chris.maldonado99@gmail.com>
2020-04-15 13:46:36 -0700

    added log txt call vars

--- liri.js
+++ liri.js
@@ -1,1 +1,26 @@
 require("dotenv").config();
+
+//vars
+var keys = require("./keys.js");
+var fs = require("fs");
+var request = require ("request");
+var Twitter = require("twitter");
+var Spotify = require('node-spotify-api');
+//creates log txt file
+var filename = './log.txt';
+
+//NPM module used to write output to console and log.txt simulatneously
+var log= require("simple-node-logger").createSimpleFileLogger(filename);
+log.setLevel('all');
+
+// argv[2] chooses users actions; argv[3] is input paramater. ie; movie title
+var userCommnad=process.argv[2];
+var secondCommand=process.argv[3];
+
+// concatenate multiple words in 2nd user arguement
+for (var i = 4; i < process.argv.length; i++) {
+    secondCommand+= '+' process.argv[i];
+}
+
+// Fetch spotify keys
+var
